692835492fc5ae45c3db1f66c7ecb1c1ac01ace3
uginim <kimugiugi@gmai.com>
2019-08-08 18:11:52 +0900

    feat: Add markdown editor for posting

--- routes/post.js
+++ routes/post.js
@@ -7,11 +7,20 @@ const { Post, Bloginfo } = require('../models');
 router.get('/', async (req, res) => {
     // const bloginfo = await Bloginfo.findOne({where:{name:'title'}});
     const posts = await Post.findAll();
+
     res.render('post', {
         title: 'Blog Title',
         posts: posts,
     });
-})
+});
+router.get('/post/:id', async (req, res, next) => {
+    const post = await Post.findOne({where:{id:req.params.id}});
+    
+    res.render('post-page',{
+        title: post.title,
+        content: post.content,
+    });
+});
 router.post('/write/post', (req, res, next) => {
     
     Post.create({
@@ -19,12 +28,12 @@ router.post('/write/post', (req, res, next) => {
         title: req.body.title,
     });
     res.redirect('/');
-})
+});
 router.get('/posting', (req, res) => {
     res.render('posting-editor',{
         title:''
     });
     
-})
+});
 
 module.exports = router;

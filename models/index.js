ce37fcc1ec68a6c1e605debf79e56d67160e5505
uginim <kimugiugi@gmail.com>
2019-08-14 23:48:33 +0900

    feat: Add aboutme page

--- models/index.js
+++ models/index.js
@@ -12,6 +12,10 @@ db.Sequelize = Sequelize;
 db.Post = require('./post')(sequelize, Sequelize);
 db.Comment = require('./comment')(sequelize, Sequelize);
 db.User = require('./user')(sequelize,Sequelize);
+db.Tag = require('./tag')(sequelize, Sequelize);
+
+db.Post.hasMany(db.Comment);
+db.Comment.belongsTo(db.Post);
 // db.Bloginfo = require('./bloginfo');
 
 

3476152f568b189fcb7e9e2acbc687fe53409b2d
uginim <kimugiugi@gmail.com>
2019-08-19 23:48:22 +0900

    feat: Add comments feature for post

--- routes/post.js
+++ routes/post.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const { Post, Bloginfo } = require('../models');
+const { Post, Bloginfo, Comment } = require('../models');
 
 router.get('/', async (req, res) => {
     // const bloginfo = await Bloginfo.findOne({where:{name:'title'}});
@@ -15,21 +15,41 @@ router.get('/', async (req, res) => {
     });
 });
 router.get('/:id', async (req, res, next) => {
-    const post = await Post.findOne({where:{id:req.params.id}});
-    
+    const post = await Post.findOne({
+        where:{id:req.params.id},
+        includes:{
+            model:Comment,
+        }
+
+    });    
     res.render('post-page',{
         title: post.title,
         content: post.content,
         user: req.user,
+        postId: req.param.id,
     });
 });
-router.post('/write/', (req, res, next) => {
+router.post('/write', (req, res, next) => {
     
     Post.create({
         content: req.body.content,
         title: req.body.title,
     });
     res.redirect('/');
+});
+router.post('/:id/comment', async (req, res, next) => {
+    req.params.id;
+    const post = await Post.findOne({
+        where:{id:req.params.id},
+        includes:{
+            model:Comment,
+        }
+
+    });  
+    post.addComments({
+        
+    });
+    
 });
 router.get('/', (req, res) => {
     res.render('posting-editor',{

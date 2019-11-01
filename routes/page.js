1dd73489c2e682a246633867889f29bc7930a11e
uginim <kimugiugi@gmail.com>
2019-11-01 23:53:52 +0900

    chore: Remove test codes for new main page

--- routes/page.js
+++ routes/page.js
@@ -89,32 +89,6 @@ router.get('/privacypolicy', async (req, res, next) => {
     }
 });
 
-router.get('/test-main',async (req, res, next) => {
-    try {
-        
-        let posts = await Post.findAll({ include:[
-            {
-                model:Tag,
-            }
-        ]});    
-        posts = posts.map((post)=>{
-            const year = post.createdAt.getFullYear()
-                ,month = post.createdAt.getMonth()
-                ,day = post.createdAt.getDate();
-            post.formatDate = `${year}-${month<10?'0':''}${month}-${day<10?'0':''}${day}`
-            return post;
-        });
-        res.render('post-test', {
-            blogTitle:await getBlogTitle(),
-            title: 'Blog Title',
-            posts: posts,
-            user: req.user,
-        });
-    } catch(error) {
-        console.error(error);
-        next(error);
-    }
-});
 
 router.get('/categories', async(req, res, next)=> {
     try {
